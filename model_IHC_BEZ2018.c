f525ccd40cc23ecdec7ebdc963f2e6d444b8c97b
Mark Saddler <msaddler@mit.edu>
2019-10-21 10:49:01 -0400

    reformat IHCAN function header

--- model_IHC_BEZ2018.c
+++ model_IHC_BEZ2018.c
@@ -44,8 +44,16 @@
 
 
 
-void IHCAN(double *px, double cf, int nrep, double tdres, int totalstim,
-           double cohc, double cihc, int species, double bandwidth_scale_factor, double *ihcout)
+void IHCAN(double *px,
+           double cf,
+           int nrep,
+           double tdres,
+           int totalstim,
+           double cohc,
+           double cihc,
+           int species,
+           double bandwidth_scale_factor,
+           double *ihcout)
 {
 
     /*variables for middle-ear model */
