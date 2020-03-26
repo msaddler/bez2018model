04fca8b6b7c23bb010cc562dac77b5d284d9a96a
Mark Saddler <msaddler@mit.edu>
2020-03-26 15:26:04 -0400

    minor whitespace change in model_Synapse_BEZ2018 header file

--- model_Synapse_BEZ2018.h
+++ model_Synapse_BEZ2018.h
@@ -1,2 +1,16 @@
-void SingleAN(double *px, double cf, int nrep, double tdres, int totalstim, double noiseType, double implnt, double spont, double tabs,
-              double trel, double *meanrate, double *varrate, double *psth, double *synout, double *trd_vector, double *trel_vector);
+void SingleAN(double *px,
+              double cf,
+              int nrep,
+              double tdres,
+              int totalstim,
+              double noiseType,
+              double implnt,
+              double spont,
+              double tabs,
+              double trel,
+              double *meanrate,
+              double *varrate,
+              double *psth,
+              double *synout,
+              double *trd_vector,
+              double *trel_vector);
