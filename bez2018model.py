10768a74772770a07e1fd38850954956534a67c0
Mark Saddler <msaddler@mit.edu>
2020-12-21 19:07:11 -0500

    redefining nervegram_spike_tensor to be sparse representation of binary spike tensor with minimum integer datatype

--- bez2018model.py
+++ bez2018model.py
@@ -1,6 +1,9 @@
 import cython_bez2018 # Package must be installed in-place: `python setup.py build_ext --inplace`
 import numpy as np
 import scipy.signal
+import sys
+import pdb
+import time
 
 
 def get_ERB_cf_list(num_cf, min_cf=125.0, max_cf=8e3):
@@ -22,6 +25,26 @@ def get_ERB_cf_list(num_cf, min_cf=125.0, max_cf=8e3):
     cf_list = np.linspace(E_start, E_end, num=num_cf)
     cf_list = (1.0/0.00437) * (10.0 ** (cf_list / 21.4) - 1.0)
     return list(cf_list)
+
+
+def get_sparse_index_min_dtype(shape):
+    '''
+    Helper function returns minimum numpy integer dataype for converting an array
+    with specified `shape` to a sparse array (list of integers).
+    '''
+    largest_dim = np.max(shape)
+    if largest_dim <= 255 + 1:
+        return np.uint8
+    elif largest_dim <= 65535 + 1:
+        return np.uint16
+    elif largest_dim <= 4294967295 + 1:
+        return np.uint32
+    elif largest_dim <= 9223372036854775807 + 1:
+        return np.int64
+    elif largest_dim <= 18446744073709551615 + 1:
+        return np.uint64
+    else:
+        raise ValueError("Requested shape too large to store indexes as integers")
 
 
 def nervegram(signal,
@@ -138,7 +161,7 @@ def nervegram(signal,
         nervegram_vihcs = []
     if return_meanrates:
         nervegram_meanrates = []
-    if return_spike_times:
+    if return_spike_times or return_spike_tensor:
         nervegram_spike_times = []
 
     # Iterate over all CFs and run the auditory nerve model components
@@ -172,7 +195,7 @@ def nervegram(signal,
             tmp_meanrate = scipy.signal.resample_poly(synapse_out['meanrate'], int(nervegram_fs), int(pin_fs))
             tmp_meanrate[tmp_meanrate < 0] = 0
             nervegram_meanrates.append(tmp_meanrate)
-        if return_spike_times:
+        if return_spike_times or return_spike_tensor:
             tmp_spike_times = synapse_out['spike_times']
             nervegram_spike_times.append(tmp_spike_times)
 
@@ -181,7 +204,7 @@ def nervegram(signal,
         nervegram_vihcs = np.stack(nervegram_vihcs, axis=0).astype(np.float32)
     if return_meanrates:
         nervegram_meanrates = np.stack(nervegram_meanrates, axis=0).astype(np.float32)
-    if return_spike_times:
+    if return_spike_times or return_spike_tensor:
         nervegram_spike_times = np.stack(nervegram_spike_times, axis=1).astype(np.float32)
 
     # ============ APPLY TRANSFORMATIONS ============ #
@@ -213,7 +236,7 @@ def nervegram(signal,
         if return_meanrates:
             nervegram_meanrates = nervegram_meanrates[:, clip_start_nervegram:clip_end_nervegram]
         # Adjust spike times (set t=0 to `clip_start_nervegram` and eliminate negative times)
-        if return_spike_times:
+        if return_spike_times or return_spike_tensor:
             clip_start_nervegram_time = clip_start_nervegram / nervegram_fs
             clip_end_nervegram_time = clip_end_nervegram / nervegram_fs
             nervegram_spike_times[nervegram_spike_times >= clip_end_nervegram_time] = 0
@@ -226,16 +249,18 @@ def nervegram(signal,
                     spike_times = spike_times[spike_times > 0]
                     nervegram_spike_times[itr0, itr1, :] = 0
                     nervegram_spike_times[itr0, itr1, 0:spike_times.shape[0]] = spike_times
-    # Generate binary tensor of spikes (with shape [spike_train, CF, time]) from spike times
+    # Generate sparse representation of binary spike tensor from spike times
     if return_spike_tensor:
-        assert return_spike_times, "`return_spike_times` must be true to generate spike tensor"
         nervegram_spike_indices = (nervegram_spike_times * nervegram_fs).astype(int)
-        out_shape = list(nervegram_spike_indices.shape)[:-1] + [int(nervegram_dur*nervegram_fs)]
-        nervegram_spike_tensor = np.zeros(out_shape, dtype=int)
+        # Binary spike tensor has dense shape [spike_train, CF, time]
+        dense_shape = np.array(list(nervegram_spike_indices.shape)[:-1] + [int(nervegram_dur*nervegram_fs)])
+        nervegram_spike_tensor = []
         for itr0 in range(nervegram_spike_indices.shape[0]):
             for itr1 in range(nervegram_spike_indices.shape[1]):
-                indices = np.trim_zeros(nervegram_spike_indices[itr0, itr1], trim='b')
-                nervegram_spike_tensor[itr0, itr1, indices] = 1
+                for itr2 in np.trim_zeros(nervegram_spike_indices[itr0, itr1], trim='b'):
+                    nervegram_spike_tensor.append([itr0, itr1, itr2])
+        nervegram_spike_tensor_dtype = get_sparse_index_min_dtype(dense_shape)
+        nervegram_spike_tensor = np.stack(nervegram_spike_tensor, axis=1).astype(nervegram_spike_tensor_dtype)
 
     # ============ RETURN OUTPUT AS DICTIONARY ============ #
     output_dict = {
@@ -272,5 +297,6 @@ def nervegram(signal,
     if return_spike_times:
         output_dict['nervegram_spike_times'] = nervegram_spike_times
     if return_spike_tensor:
+        output_dict['nervegram_spike_tensor_shape'] = dense_shape
         output_dict['nervegram_spike_tensor'] = nervegram_spike_tensor
     return output_dict
